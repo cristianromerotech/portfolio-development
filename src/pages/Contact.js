c2fd7b74b26cb38be1dc05463f22023fc0e6e2b5
cristianfernandoromero1 <cfomero99@gmail.com>
2024-06-28 20:10:06 -0500

    format code

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,31 +1,53 @@
+import React from "react";
 import { TextField, Container, Grid, Button, Box } from "@mui/material";
-//import background_blue from "../images/background_blue.jpg";
-import "../App.css";
-import { grey } from "@mui/material/colors";
-const accent = grey["A100"];
 
 function Contact() {
   return (
-    <div /*  style={{ backgroundImage: `url(${background_blue})` }}  */>
+    <div>
+      {/* No se necesita la imagen de fondo */}
       <Container fixed>
-        <h1>Contact</h1>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <TextField id="outlined-basic" label="Correo" variant="outlined" />
+        <Grid container spacing={1}>
+          <Grid item xs={2}></Grid>
+          <Grid item xs={8} align="center">
+            <Box
+              height={300}
+              width={200}
+              my={4}
+              display="flex"
+              alignItems="center"
+              gap={4}
+              p={2}
+              sx={{ border: "2px solid grey" }}
+              bgcolor="primary.dark"
+            >
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <h1>Contact</h1>
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    id="outlined-basic"
+                    label="Correo"
+                    variant="outlined"
+                    color="success"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    id="outlined-basic"
+                    label="Mensaje"
+                    variant="outlined"
+                    multiline
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <Button variant="contained">Send</Button>
+                </Grid>
+              </Grid>
+            </Box>
           </Grid>
-          <Grid item xs={12}>
-            <TextField
-              id="outlined-basic"
-              label="Mensaje"
-              variant="outlined"
-              multiline
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <Button variant="contained">Send</Button>
-          </Grid>
+          <Grid item xs={2}></Grid>
         </Grid>
-        <br />
       </Container>
     </div>
   );
